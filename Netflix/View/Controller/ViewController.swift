96dac50af77b4c0733584367931c0ccb2d125177
Kayky Casagrande <kayky.casagrande@unicred.com.br>
2022-12-20 11:34:58 -0300

    First part

--- Netflix/View/Controller/ViewController.swift
+++ Netflix/View/Controller/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+	let MOVIES_PER_PAGE = 18
 
+	@IBOutlet weak var movieList: UILabel!
+	
+	private let viewModel = MovieViewModel()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		
+		viewModel.getMovies { movies in
+			var titles = ""
+			
+			let slice = movies[0..<self.MOVIES_PER_PAGE]
+			
+			for movie in slice {
+				titles += "- \(movie.title)\n\tDuration: \(Int(movie.duration / 60)) min\n\n"
+			}
+			
+			self.movieList.numberOfLines = slice.count * 2
+			self.movieList.text = titles
+		}
 	}
-
-
 }
 
